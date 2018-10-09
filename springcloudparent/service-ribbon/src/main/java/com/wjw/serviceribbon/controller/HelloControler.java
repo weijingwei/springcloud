dd42dd3f28382a1ddc00ac85c937aa0aa3b40f66
weijingwei <weijingwei@weijingwei-PC>
2018-10-09 21:27:48 +0800

    init

---
+++ springcloudparent/service-ribbon/src/main/java/com/wjw/serviceribbon/controller/HelloControler.java
@@ -0,0 +1,20 @@
+package com.wjw.serviceribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wjw.serviceribbon.service.HelloService;
+
+@RestController
+public class HelloControler {
+
+	@Autowired
+	HelloService helloService;
+
+	@GetMapping(value = "/hi")
+	public String hi(@RequestParam String name) {
+		return helloService.hiService(name);
+	}
+}
