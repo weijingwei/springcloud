380681d778debddc0dbedefa3269e85c62c76dc4
weijingwei <weijingwei@weijingwei-PC>
2018-10-09 22:47:06 +0800

    init

--- springcloudparent/service-ribbon/src/main/java/com/wjw/serviceribbon/service/HelloService.java
+++ springcloudparent/service-ribbon/src/main/java/com/wjw/serviceribbon/service/HelloService.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class HelloService {
 
-    @Autowired
-    private RestTemplate restTemplate;
+	@Autowired
+	private RestTemplate restTemplate;
 
-    public String hiService(String name) {
-        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
-    }
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String hiService(String name) {
+		return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+	}
 
+	public String hiError(String name) {
+		return "hi," + name + ",sorry,error!";
+	}
 
 }
