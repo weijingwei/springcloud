60ba5f48a3341457e423ae59d2585f353d2946e6
weijingwei <weijingwei@weijingweidembp.cn.ibm.com>
2018-10-10 11:30:46 +0800

    init

---
+++ springcloudparent/service-zuul/src/test/java/com/wjw/servicezuul/ServiceZuulApplicationTest.java
@@ -0,0 +1,38 @@
+package com.wjw.servicezuul;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ServiceZuulApplicationTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ServiceZuulApplicationTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ServiceZuulApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
