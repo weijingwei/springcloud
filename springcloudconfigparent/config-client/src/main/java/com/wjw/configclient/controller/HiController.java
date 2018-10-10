ddadf5500885176b65c8ae6fb906ccbe90bcc640
weijingwei <weijingwei@weijingwei-PC>
2018-10-10 23:56:20 +0800

    merge

--- springcloudconfigparent/config-client/src/main/java/com/wjw/configclient/controller/HiController.java
+++ springcloudconfigparent/config-client/src/main/java/com/wjw/configclient/controller/HiController.java
@@ -1,11 +1,16 @@
 package com.wjw.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HiController {
+	/**
+	 * http://localhost:8881/actuator/bus-refresh
+	 */
 
 	@Value("${foo}")
 	String foo;
