325b739353de141faa9d3837e76a78584e427992
weijingwei <weijingwei@weijingweidembp.cn.ibm.com>
2018-10-16 16:15:16 +0800

    init

--- springcloudparent/service-hi/src/main/java/com/wjw/servicehi/controller/HomeController.java
+++ springcloudparent/service-hi/src/main/java/com/wjw/servicehi/controller/HomeController.java
@@ -5,13 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class HomeController {
 	@Value("${server.port}")
 	String port;
 
 	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod = "hiError")
 	public String home(@RequestParam(value = "name", defaultValue = "weijingwei") String name) {
 		return "hi " + name + " ,i am from port:" + port;
 	}
+
+	public String hiError(String name) {
+		return "hi," + name + ",sorry,error!";
+	}
+
 }
